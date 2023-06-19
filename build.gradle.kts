58b2a5be8ba06f41165c71e032ab9c4523e5b78c
oceenachi <phillipfrancisca.com>
2023-06-19 14:35:05 +0100

    QALI-466: publish the oss artifacts to sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,8 +110,11 @@ allprojects {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    signing {
-        publishing.publications.forEach { sign(it) }
+    val signingKeyId = "signing.keyId"
+    if (System.getProperty(signingKeyId) != null || System.getenv(signingKeyId) != null) {
+        signing {
+            publishing.publications.forEach { sign(it) }
+        }
     }
 
     val ossrhUsername: String? by project
